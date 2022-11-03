07b7f803cf7b1e3849bfc204780c80377fe43902
TromuDev <110989200+TromuDev@users.noreply.github.com>
2022-11-03 20:04:05 -0300

    Add files via upload
    Ejemplo realizado en la clase del día 03/11
    
    1. calculadora_suma.php: formulario donde se solicita al usuario que ingrese 2 valores para ser sumados
    2. suma_resultado.php: código PHP que realiza la sumatoria de los 2 valores ingresados a través del formulario anterior. Finalmente muestra el resultado por pantalla

---
+++ calculadora_suma.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Calculadora Suma</title>
+</head>
+<body>
+    <div>
+        <form action="suma_resultado.php" method="GET">
+            Ingrese dos valores para sumar:
+            <br>
+            <input type="text" name="number_1"> 
+            +
+            <input type="text" name="number_2">
+            <input type="submit" value="enviar">
+        </form>
+    </div>
+</body>
+</html>
---
+++ suma_resultado.php
@@ -0,0 +1,7 @@
+<?php
+$valor1 = $_GET['number_1'];
+$valor2 = $_GET['number_2'];
+$resultado = $valor1 + $valor2;
+
+echo "La suma de los valores es: ".$resultado;
+?>
